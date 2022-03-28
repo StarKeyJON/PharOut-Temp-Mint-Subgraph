320648cf24afb8d302a0a03ebcd88ff598b56918
phunkyjon.eth <83514596+StarKeyJON@users.noreply.github.com>
2022-03-28 19:49:45 -0400

    Update mapping.ts

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,81 +1,92 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Address, ipfs } from "@graphprotocol/graph-ts"
 import {
   nftClaimed, TempMint
 } from "../generated/TempMint/TempMint"
-import { Mint, NFT } from "../generated/schema"
+import { Mint, NFT, Metadata } from "../generated/schema"
+import { PhamNFTs, Transfer } from "../generated/PhamNFTs/PhamNFTs";
 
 export function handlenftClaimed(event: nftClaimed): void {
 
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let mint = Mint.load(event.transaction.from.toHex())
-
-  let contract = TempMint.bind(event.address)
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
+  let mint = Mint.load(event.transaction.from.toHex());
   if (!mint) {
     mint = new Mint(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
     mint.count = BigInt.fromI32(0)
   }
 
-  let price = contract.fetchMintPrice()
+  let contract = TempMint.bind(event.address);
+  let nftContract = PhamNFTs.bind(Address.fromString("0x851A7c43C148bfA7F79dAA312d09A61306B56006"));
 
-  mint.price = price
+  let nft = NFT.load(nftContract._address.toString() + event.params.nftId.toString());
+  if(!nft){
+    nft = new NFT(nftContract._address.toString() + event.params.nftId.toString());
+  }
+  nft.contract_type = "ERC721";
+  nft.token_address = nftContract._address;
+  nft.token_id = event.params.nftId
+  nft.owner_of = event.params.receiver
+  
+  let metadata = Metadata.load(nftContract._address.toString()+event.params.nftId.toString());
+  if(!metadata){
+    metadata = new Metadata(nftContract._address.toString()+event.params.nftId.toString());
+  }
+  metadata.type = "ERC721";
+  let uri = nftContract.try_tokenURI(event.params.nftId);
+  if(uri){
+    metadata.uri = uri.value;
+  }
+  let name = nftContract.try_name();
+  if(name){
+    metadata.name = name.value;
+  }
+  let symbol = nftContract.try_symbol();
+  if(symbol){
+    metadata.symbol = symbol.value;
+  }
+  metadata.save();
 
-  // BigInt and BigDecimal math are supported
-  mint.count = mint.count + BigInt.fromI32(1)
+  let price = contract.fetchMintPrice();
+  mint.price = price;
 
-  // Entity fields can be set based on event parameters
+  mint.count = mint.count + BigInt.fromI32(1);
+
   mint.minter = event.params.minter
   mint.receiver = event.params.receiver
-
-  // Entities can be written to the store with `.save()`
+  mint.token_id = event.params.nftId;
+  
   mint.save()
-
-  let nft = NFT.load(event.transaction.from.toHex())
-
-  if (!nft) {
-    nft = new NFT(event.transaction.from.toHex())
-  }
-
-    // Entity fields can be set based on event parameters
-    nft.owner_of = event.params.receiver
-    nft.token_id = event.params.nftId
-
   nft.save()
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+}
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.DEV_ROLE(...)
-  // - contract.fetchMintPrice(...)
-  // - contract.fetchNFTsCreated(...)
-  // - contract.fetchNFTsCreatedByAddress(...)
-  // - contract.fetchNFTsCreatedCount(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.hasRole(...)
-  // - contract.mintPrice(...)
-  // - contract.nftAddress(...)
-  // - contract.nftsRedeemed(...)
-  // - contract.onERC1155Received(...)
-  // - contract.onERC721Received(...)
-  // - contract.paused(...)
-  // - contract.setMintPrice(...)
-  // - contract.setNftAddress(...)
-  // - contract.supportsInterface(...)
+export function handleTransfer(event: Transfer): void {
+
+let nftContract = PhamNFTs.bind(Address.fromString("0x851A7c43C148bfA7F79dAA312d09A61306B56006"));
+  let nft = NFT.load(nftContract._address.toString() + event.params.tokenId.toString());
+  if(!nft){
+    nft = new NFT(nftContract._address.toString() + event.params.tokenId.toString());
+  }
+  nft.contract_type = "ERC721";
+  nft.token_address = nftContract._address;
+  nft.token_id = event.params.tokenId
+  nft.owner_of = event.params.to
+  
+  let metadata = Metadata.load(nftContract._address.toString()+event.params.tokenId.toString());
+  if(!metadata){
+    metadata = new Metadata(nftContract._address.toString()+event.params.tokenId.toString());
+  }
+  metadata.type = "ERC721";
+  let uri = nftContract.try_tokenURI(event.params.tokenId);
+  if(uri){
+    metadata.uri = uri.value;
+  }
+  let name = nftContract.try_name();
+  if(name){
+    metadata.name = name.value;
+  }
+  let symbol = nftContract.try_symbol();
+  if(symbol){
+    metadata.symbol = symbol.value;
+  }
+  metadata.save();
+  nft.save();
 
 }
